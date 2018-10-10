1ad7dd41ab65f24246a03f4ec8d6f9b9f9229f7f
oleynik.a.o <oleynik.a.o@gmail.com>
2018-10-11 02:37:09 +0300

    lab1: part 1,2

---
+++ lab1_XOR/lab1_XOR/Decoders.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_XOR
+{
+    class Decoders
+    {
+        public void XORDecoder(string input)
+        {
+            int key = 19;
+            char[] output = new char[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                output[i] = (char)(input[i] ^ key);
+                Console.Write(output[i]);
+            }
+        }
+
+        public string ToHexString(string text)
+        {
+            string hex = "";
+            byte[] inputBytes = Encoding.Default.GetBytes(text);
+            foreach (byte b in inputBytes)
+            {
+                hex += String.Format("{0:x2}", b);
+            }
+            Console.WriteLine($"hex: {hex}");
+            return hex;
+        }
+
+        public byte[] FromHexString(string text, int start)
+        {
+            byte[] bytes = new byte[text.Length / 2 - start];
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = Convert.ToByte(text.Substring((i + start) * 2, 2), 16);
+            }
+
+            return bytes;
+        }
+
+        public int HammingDistance(byte[] b1, byte[] b2)
+        {
+            int distance = 0;
+
+            for (int i = 0; i < b2.Length; i++)
+            {
+                if (b1[i] != b2[i])
+                {
+                    distance++;
+                }
+            }
+
+            return distance;
+        }
+
+        public byte[] Devision(byte[] b, int start)
+        {
+            byte[] answer = new byte[b.Length / 3];
+            int j = start;
+            for (int i = 0; i < answer.Length; i++)
+            {
+                answer[i] = b[j];
+                j += 3;
+            }
+
+            return answer;
+        }
+
+        public Dictionary<byte, int> LetterFrequancy(byte[] b)
+        {
+            Dictionary<byte, int> freq = new Dictionary<byte, int>();
+            freq = b.GroupBy(x => x).OrderByDescending(x => x.Count()).ToDictionary(x => x.Key, x => x.Count());
+            foreach (var c in freq)
+            {
+                // Console.WriteLine($" {c.Key}      {c.Value}");
+            }
+
+            return freq;
+        }
+
+        public void Decoder2(byte[] b)
+        {
+            byte[] f = new byte[9];
+            for (int i = 0; i < 9; i++)
+            {
+                f[i] = b[i];
+                //Console.WriteLine($"{f[i]}");
+            }
+
+            int f1 = f[0];
+            int f2 = f[1];
+            int f3 = f[2];
+            int f4 = f[3];
+            int f5 = f[4];
+            int f6 = f[5];
+            int f7 = f[6];
+            int f8 = f[7];
+            int f9 = f[8];
+            int k1 = f1 ^ 32;
+            int k2 = f2 ^ (int)'e';
+            int k3 = f3 ^ (int)'o';
+            int k4 = f4 ^ (int)'i';
+            int k5 = f5 ^ (int)'i';
+            int k6 = f6 ^ (int)'s';
+            int k7 = f7 ^ (int)'5';
+            int k8 = f8 ^ (int)'1';
+            int k9 = f9 ^ (int)'u';
+            Console.WriteLine($"b1: {b[0]} k1: {k1} char: {(char)k1}");
+            Console.WriteLine($"b1: {b[1]} k2: {k2} char: {(char)k2}");
+            Console.WriteLine($"b1: {b[2]} k3: {k3} char: {(char)k3}");
+            Console.WriteLine($"b1: {b[3]} k4: {k4} char: {(char)k4}");
+            Console.WriteLine($"b1: {b[4]} k5: {k5} char: {(char)k5}");
+            Console.WriteLine($"b1: {b[5]} k6: {k6} char: {(char)k6}");
+            Console.WriteLine($"b1: {b[6]} k7: {k7} char: {(char)k7}");
+            Console.WriteLine($"b1: {b[7]} k8: {k8} char: {(char)k8}");
+            Console.WriteLine($"b1: {b[8]} k9: {k9} char: {(char)k9}");
+        }
+
+        public void Vigener(byte[] b, string key)
+        {
+            int[] answer = new int[b.Length];
+            char[] c = key.ToCharArray();
+            int j = 0;
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (j == 3)
+                {
+                    j = 0;
+                }
+                answer[i] = (int)b[i] ^ (int)c[j];
+                Console.Write($" {(char)answer[i]}");
+
+                j++;
+            }
+        }
+    }
+}
---
+++ lab1_XOR/lab1_XOR/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_XOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Decoders task1 = new Decoders();
+
+            task1.XORDecoder(@"]|d3gaj3r3avcvrgz}t>xvj3K\A3pzc{va=3V=t=3zg3`{|f.w3grxv3r3`gaz}t31{v..|3d|a.w13r}w?3tzev}3g{v3xvj3z`31xvj1?3k|a3g{v3uza`g3.vggva31{13dzg{31x1?3g{v}3k|a31v13dzg{31v1?3g{v}31.13dzg{31j1?3r}w3g{v}3k|a3}vkg3p{ra31.13dzg{31x13rtrz}?3g{v}31|13dzg{31v13r}w3`|3|}=3J|f3~rj3f`v3z}wvk3|u3p|z}pzwv}pv?3[r~~z}t3wz`gr}pv?3Xr`z`xz3vkr~z}rgz|}?3`grgz`gzpr.3gv`g`3|a3d{rgveva3~vg{|w3j|f3uvv.3d|f.w3`{|d3g{v3qv`g3av`f.g=");
+
+            byte[] b = task1.FromHexString("1c41023f564b2a130824570e6b47046b521f3f5208201318245e0e6b40022643072e13183e51183f5a1f3e4702245d4b285a1b23561965133f2413192e571e28564b3f5b0e6b50042643072e4b023f4a4b24554b3f5b0238130425564b3c564b3c5a0727131e38564b245d0732131e3b430e39500a38564b27561f3f5619381f4b385c4b3f5b0e6b580e32401b2a500e6b5a186b5c05274a4b79054a6b67046b540e3f131f235a186b5c052e13192254033f130a3e470426521f22500a275f126b4a043e131c225f076b431924510a295f126b5d0e2e574b3f5c4b3e400e6b400426564b385c193f13042d130c2e5d0e3f5a086b52072c5c192247032613433c5b02285b4b3c5c1920560f6b47032e13092e401f6b5f0a38474b32560a391a476b40022646072a470e2f130a255d0e2a5f0225544b24414b2c410a2f5a0e25474b2f56182856053f1d4b185619225c1e385f1267131c395a1f2e13023f13192254033f13052444476b4a043e131c225f076b5d0e2e574b22474b3f5c4b2f56082243032e414b3f5b0e6b5d0e33474b245d0e6b52186b440e275f456b710e2a414b225d4b265a052f1f4b3f5b0e395689cbaa186b5d046b401b2a500e381d", 0);
+
+            //Console.WriteLine($"size {b.Length}");
+
+            /* byte[] b1 = task1.FromHexString("1c41023f564b2a130824570e6b47046b521f3f5208201318245e0e6b40022643072e13183e51183f5a1f3e4702245d4b285a1b23561965133f2413192e571e28564b3f5b0e6b50042643072e4b023f4a4b24554b3f5b0238130425564b3c564b3c5a0727131e38564b245d0732131e3b430e39500a38564b27561f3f5619381f4b385c4b3f5b0e6b580e32401b2a500e6b5a186b5c05274a4b79054a6b67046b540e3f131f235a186b5c052e13192254033f130a3e470426521f22500a275f126b4a043e131c225f076b431924510a295f126b5d0e2e574b3f5c4b3e400e6b", 0);
+             Console.WriteLine();
+           //  task1.Example();
+             for (int i = 0; i <223; i++)
+                 {
+                 byte[] b2 = task2.FromHexString("400426564b385c193f13042d130c2e5d0e3f5a086b52072c5c192247032613433c5b02285b4b3c5c1920560f6b47032e13092e401f6b5f0a38474b32560a391a476b40022646072a470e2f130a255d0e2a5f0225544b24414b2c410a2f5a0e25474b2f56182856053f1d4b185619225c1e385f1267131c395a1f2e13023f13192254033f13052444476b4a043e131c225f076b5d0e2e574b22474b3f5c4b2f56082243032e414b3f5b0e6b5d0e33474b245d0e6b52186b440e275f456b710e2a414b225d4b265a052f1f4b3f5b0e395689cbaa186b5d046b401b2a500e381d", i);
+                 Console.WriteLine($"\ndistance: {task1.HammingDistance(b1, b2)}, number b2: {b2.Length}     diff: {b2.Length - task1.HammingDistance(b1, b2)}");
+             }*/
+
+            byte[] b1 = task1.Devision(b, 0);
+            byte[] b2 = task1.Devision(b, 1);
+            byte[] b3 = task1.Devision(b, 2);
+           
+            byte[] b4 = task1.LetterFrequancy(b3).Keys.ToArray();
+            // task1.Decoder2(b4);
+            task1.Vigener(b, "K3k");
+            // Console.WriteLine($"b2:");
+            // task1.LetterFrequancy(b2);
+            // Console.WriteLine($"b3:");
+            // task1.LetterFrequancy(b3);
+        }
+
+
+    }
+}
